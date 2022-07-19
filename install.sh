0d2de7cdc1f09feb140e177a087278474a42ee68
Power-Bot-FX <109558880+Power-Bot-FX@users.noreply.github.com>
2022-07-18 23:07:18 -0300

    Install.sh

--- install.sh
+++ install.sh
@@ -47,11 +47,5 @@ npm i fb-video-downloader
 
 echo -e '\e[1;32m
 Descarga Completada al 100%
-Creador: Thechoute
-Numero: +18299897014
-Instagram: the_choute_
-Youtube: https://www.youtube.com/c/thechout
-
-Escribe node thechoute para iniciar el bot
-En caso que el bot presente algun problema, comunicate con el creador su numero esta mas arriba
-'
+Creador: leonardo
+Numero: +5493772642151
